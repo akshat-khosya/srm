04f2904e65728ae9095ed58eb38c3f8a49d6f1ef
Akshat Mittal <akshatmittal2506@gmail.com>
2022-06-13 09:05:59 +0530

    fixed import

--- src/pages/Groups/Group.jsx
+++ src/pages/Groups/Group.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../../context/Context";
 import groupFallbackIcon from "../../Images/group_icon.svg";
 import AddUsers from "./AddUsers";
