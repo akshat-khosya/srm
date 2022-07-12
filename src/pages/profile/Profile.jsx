23bd8b8248ccb7873b39e7aa18d16c4e84db166a
Akshat Mittal <akshatmittal2506@gmail.com>
2022-07-12 17:18:36 +0530

    restructured the profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,7 +5,7 @@ import "./profile.css";
 
 import SnackBar from "../../components/Snackbar";
 function Profile({ axiosInstance }) {
-	const [edit, setEdit] = useState(true);
+	const [edit, setEdit] = useState(false);
 	const { user, dispatch } = useContext(Context);
 	const [err, setErr] = useState({
 		status: false,
@@ -42,18 +42,15 @@ function Profile({ axiosInstance }) {
 			dispatch({ type: "LOGIN_FAILURE" });
 		}
 	};
-	const handelChange = (e) => {
+	const handleChange = (e) => {
 		const { name, value } = e.target;
-
+		console.log(name, value);
 		setPersonal((prevValue) => {
 			return {
 				...prevValue,
 				[name]: value,
 			};
 		});
-	};
-	const changeProfile = () => {
-		setEdit(!edit);
 	};
 	const handelSubmit = async (e) => {
 		e.preventDefault();
@@ -84,7 +81,7 @@ function Profile({ axiosInstance }) {
 				console.log(res.data);
 				loadData();
 				setErr({ status: true, message: "Saved" });
-				setEdit(!edit);
+				setEdit(false);
 				setErr({ status: false });
 				console.log(user);
 			} else {
@@ -104,8 +101,373 @@ function Profile({ axiosInstance }) {
 			console.log(err);
 		}
 	};
-
 	return (
+		<>
+			<div className="profile-container">
+				<div className="profile-box">
+					<div className="profile-head">
+						<span>My Profile</span>
+						<button
+							className="profile-head-btn icon"
+							onClick={() => setEdit((p) => !p)}
+						>
+							<span className="material-icons">
+								{edit ? "close" : "edit"}
+							</span>
+						</button>
+					</div>
+					<div className="profile-body">
+						<form className="profile-form">
+							<div className="Row">
+								<div className="Col-lg-40 Col-md-40 Col-sm-100">
+									<div
+										className="profile-form-group"
+										style={{
+											justifyContent: "flex-start",
+											margin: "1rem 0",
+										}}
+									>
+										<input
+											disabled={!edit}
+											required
+											className="profile-input profile-input-img dispn"
+											type="file"
+											id="file"
+										/>
+										<label htmlFor="file">
+											<img
+												src={
+													edit
+														? file
+															? URL.createObjectURL(
+																	file
+															  )
+															: `${axiosInstance.defaults.baseURL}images/${user.photo}`
+														: `${axiosInstance.defaults.baseURL}images/${user.photo}`
+												}
+												alt={user.name}
+											/>
+											{edit && (
+												<span className="profile-form-input-img-edit material-icons icon">
+													edit
+												</span>
+											)}
+										</label>
+									</div>
+								</div>
+								<div className="Col-lg-50 Col-md-50 Col-sm-100">
+									<div className="profile-form-group">
+										<input
+											placeholder="Name"
+											value={personal.name}
+											disabled
+											required
+											type="text"
+											id="name"
+											name="name"
+											onChange={handleChange}
+											title="Name"
+										/>
+										<div
+											className="profile-form-input-group"
+											title="Email"
+											style={{
+												width: "100%",
+												margin: "0",
+											}}
+										>
+											<label htmlFor="email">
+												<span className="material-icons">
+													email
+												</span>
+											</label>
+											<input
+												placeholder="Email"
+												value={user.email}
+												disabled={!edit}
+												required
+												type="email"
+												id="email"
+												name="email"
+												onChange={handleChange}
+											/>
+										</div>
+										{user.phone !== "" && (
+											<div
+												className="profile-form-input-group"
+												title="Phone No."
+												style={{ width: "49%" }}
+											>
+												<label htmlFor="phone">
+													<span className="material-icons">
+														phone
+													</span>
+												</label>
+												<input
+													placeholder="Phone No."
+													value={personal.phone}
+													disabled={!edit}
+													required
+													type="tel"
+													id="phone"
+													name="phone"
+													onChange={handleChange}
+												/>
+											</div>
+										)}
+										<div
+											className="profile-form-input-group"
+											title="Username"
+											style={{ width: "49%" }}
+										>
+											<label htmlFor="username">
+												<span className="material-icons">
+													account_circle
+												</span>
+											</label>
+											<input
+												placeholder="Username"
+												value={personal.username}
+												disabled={!edit}
+												required
+												type="text"
+												id="username"
+												name="username"
+												onChange={handleChange}
+											/>
+										</div>
+										<div
+											className="profile-form-input-group"
+											title="Short Bio"
+											style={{ width: "100%" }}
+										>
+											<label htmlFor="username">
+												<span className="material-icons">
+													account_circle
+												</span>
+											</label>
+											<input
+												placeholder="Short Bio"
+												value={personal.bio}
+												disabled={!edit}
+												required
+												type="text"
+												id="bio"
+												name="bio"
+												onChange={handleChange}
+											/>
+										</div>
+									</div>
+								</div>
+								<div className="Col-lg-10 Col-md-10 Col-sm-100"></div>
+								<div className="Col-lg-33 Col-md-50 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Date Of Birth"
+									>
+										<label htmlFor="dob">
+											<span className="material-icons">
+												today
+											</span>
+										</label>
+										<input
+											placeholder="Date Of Birth"
+											value={personal.dob}
+											disabled={!edit}
+											required
+											type="text"
+											id="dob"
+											name="dob"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+								<div className="Col-lg-33 Col-md-50 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Gender"
+									>
+										<label htmlFor="gender">
+											<span className="material-icons">
+												{user.gender === "Male"
+													? "man"
+													: "woman"}
+											</span>
+										</label>
+										<input
+											placeholder="Gender"
+											value={personal.gender}
+											disabled={!edit}
+											required
+											type="text"
+											id="gender"
+											name="gender"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+								<div className="Col-lg-33 Col-md-50 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Father's Name"
+									>
+										<label htmlFor="fname">
+											<span className="material-icons">
+												supervisor_account
+											</span>
+										</label>
+										<input
+											placeholder="Father's Name"
+											value={personal.fname}
+											disabled={!edit}
+											required
+											type="text"
+											id="fname"
+											name="fname"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+								<div className="Col-lg-33 Col-md-50 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Mother's Name"
+									>
+										<label htmlFor="mname">
+											<span className="material-icons">
+												supervisor_account
+											</span>
+										</label>
+										<input
+											placeholder="Mother's Name"
+											value={personal.mname}
+											disabled={!edit}
+											required
+											type="text"
+											id="mname"
+											name="mname"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+								<div className="Col-lg-33 Col-md-50 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Current Organization"
+									>
+										<label htmlFor="currently">
+											<span className="material-icons">
+												business
+											</span>
+										</label>
+										<input
+											placeholder="Current Organization"
+											value={personal.currentoriginaztion}
+											disabled={!edit}
+											required
+											type="text"
+											id="currently"
+											name="currently"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+								<div className="Col-lg-33 Col-md-50 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Designation"
+									>
+										<label htmlFor="work">
+											<span className="material-icons">
+												badge
+											</span>
+										</label>
+										<input
+											placeholder="Designation"
+											value={personal.desgination}
+											disabled={!edit}
+											required
+											type="text"
+											id="work"
+											name="work"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+								<div className="Col-lg-33 Col-md-50 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Batch"
+									>
+										<label htmlFor="batch">
+											<span className="material-icons">
+												date_range
+											</span>
+										</label>
+										<input
+											placeholder="Batch"
+											value={personal.batch}
+											disabled={!edit}
+											required
+											type="text"
+											id="batch"
+											name="batch"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+								<div className="Col-lg-33 Col-md-50 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Program"
+									>
+										<label htmlFor="course">
+											<span className="material-icons">
+												cast_for_education
+											</span>
+										</label>
+										<input
+											placeholder="Program"
+											value={personal.program}
+											disabled={!edit}
+											required
+											type="text"
+											id="course"
+											name="course"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+								<div className="Col-lg-33 Col-md-100 Col-sm-100">
+									<div
+										className="profile-form-input-group"
+										title="Passing Year"
+									>
+										<label htmlFor="pyear">
+											<span className="material-icons">
+												school
+											</span>
+										</label>
+										<input
+											placeholder="Passing Year"
+											value={personal.pyear}
+											disabled={!edit}
+											required
+											type="text"
+											id="pyear"
+											name="pyear"
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+	/* return (
 		<div className="home">
 			<div className="home-main">
 				<div className="profile">
@@ -680,7 +1042,7 @@ function Profile({ axiosInstance }) {
 				</div>
 			</div>
 		</div>
-	);
+	); */
 }
 
 export default Profile;
